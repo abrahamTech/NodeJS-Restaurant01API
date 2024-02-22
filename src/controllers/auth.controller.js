4019a3b29270dbd0ad53808280a4fa18709c12b6
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-21 21:14:09 -0600

    feat(Auth Controller): encrypt password and send json with user information

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,29 @@
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
+
     const {username, email, password} = req.body;
     
     try {
+
+        const passwordHash = await bcrypt.hash(password,10);
+
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash,
         });
     
-        await newUser.save();
+        const userSaved = await newUser.save();
 
-        res.send("Usuario Registrado");
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email:userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        });
 
     } catch (error) {
         console.log(error);
