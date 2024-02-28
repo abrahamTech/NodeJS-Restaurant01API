bd531cde777416c8d865e73095bf95ee392460c5
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-27 22:46:56 -0600

    feat(orders Controller): add basic functions for orders controller

---
+++ src/controllers/orders.controller.js
@@ -0,0 +1,5 @@
+export const getOrders = async (req, res) => {};
+export const createOrder = async (req, res) => {};
+export const getOrder = async (req, res) => {};
+export const updateOrder = async (req, res) => {};
+export const deleteOrder = async (req, res) => {};
--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/validateToken.js";
+import {getOrders, getOrder, createOrder, updateOrder, deleteOrder} from "../controllers/orders.controller.js"
 
 const router = Router();
 
-router.get("/orders", authenticateToken, );
-router.get("/orders/:id", authenticateToken,);
-router.post("/orders", authenticateToken,);
-router.delete("/orders/:id", authenticateToken,);
-router.put("/orders/:id", authenticateToken,);
+router.get("/orders", authenticateToken, getOrders);
+router.get("/orders/:id", authenticateToken, getOrder);
+router.post("/orders", authenticateToken, createOrder);
+router.delete("/orders/:id", authenticateToken, deleteOrder);
+router.put("/orders/:id", authenticateToken, updateOrder);
+
 export default router;
