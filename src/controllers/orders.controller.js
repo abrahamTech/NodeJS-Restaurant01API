b114d88bea102f489924321f90bdc76af65c2daa
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-28 22:45:07 -0600

    feat(orders Controller): add basic functions to test the correct order creation

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -1,5 +1,26 @@
-export const getOrders = async (req, res) => {};
-export const createOrder = async (req, res) => {};
+import Order from "../models/order.model.js";
+
+export const getOrders = async (req, res) => {
+    const orders = await Order.find();
+    res.json(orders);
+};
+
+export const createOrder = async (req, res) => {
+    const { dishName, price, clientRequests, orderDate, orderStatus } = req.body;
+
+    const newOrder = new Order({
+        dishName,
+        price,
+        clientRequests,
+        orderDate,
+        orderStatus
+    });
+
+    const savedOrder = await newOrder.save();
+
+    res.json(savedOrder);
+};
+
 export const getOrder = async (req, res) => {};
 export const updateOrder = async (req, res) => {};
 export const deleteOrder = async (req, res) => {};
