2f5ab011ce561a242a945b7662487ea3b2b07a56
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-02-29 21:25:15 -0600

    feat(orders Controller): add get, update and delelte order basic functions

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -21,6 +21,26 @@ export const createOrder = async (req, res) => {
     res.json(savedOrder);
 };
 
-export const getOrder = async (req, res) => {};
-export const updateOrder = async (req, res) => {};
-export const deleteOrder = async (req, res) => {};
+export const getOrder = async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if(!order) return res.status(404).json({message: "Orden not found"});
+
+    res.json(order);
+};
+
+export const updateOrder = async (req, res) => {
+    const order = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true, });
+
+    if(!order) return res.status(404).json({message: "Orden not found"});
+
+    res.json(order);
+};
+
+export const deleteOrder = async (req, res) => {
+    const order = await Order.findByIdAndDelete(req.params.id);
+
+    if(!order) return res.status(404).json({message: "Orden not found"});
+
+    res.json(order);
+};
