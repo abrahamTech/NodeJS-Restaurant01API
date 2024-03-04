3a13c13abeece62715a456754183f6848034f751
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-03-03 22:22:18 -0600

    fix(validateSchema): fix schema.parse method

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,8 +1,8 @@
 export const validateSchema = (schema) => (req, res, next) => {
     try {
-        schema.pase(req.body);
+        schema.parse(req.body);
         next();
     } catch (error) {
-        return res.status(400).json({ error });
+        return res.status(400).json({error});
     }
 };
