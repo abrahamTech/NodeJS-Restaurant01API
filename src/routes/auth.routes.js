b6dcca19985fcd07a99baf44038484b88739676f
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-03-03 22:01:55 -0600

    feat(validateSchema): implement validateSchema middleware in auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { login, logout, profile, register } from "../controllers/auth.controller.js";
 import { authenticateToken } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
 router.get("/profile", authenticateToken, profile);
 
