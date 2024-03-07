08f27f8a1e5968bde25f7cfd5be97ddc61c2aa7a
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-03-06 22:22:11 -0600

    feat(main JS): add preloader event listener

---
+++ client/assets/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Preload
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", () => {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+})
