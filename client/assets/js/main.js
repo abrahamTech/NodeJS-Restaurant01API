4b7cad2c15fd4d04d12f88838636cd8b22b10e36
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-03-11 21:40:08 -0600

    feat(Navbar Menu): active navbar menu when clcik menu button

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -6,4 +6,28 @@ const preloader = document.querySelector("[data-preload]");
 window.addEventListener("load", () => {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
-})
+});
+
+
+// Event listener on multiple events
+
+const addEventOnElements = function(elements, eventType, callback) {
+    for(let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+//NAVBAR
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
