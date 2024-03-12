7595ca6c762c9b1ee2d1daaa1fad8c4eacc2305a
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-03-11 22:00:27 -0600

    feat(Header): hide header

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -31,3 +31,30 @@ const toggleNavbar = function () {
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+//HEADER
+
+const header = document.querySelector("[data-header]");
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+    const isScrollBottom = lastScrollPos < window.scrollY;
+
+    if(isScrollBottom) {
+        header.classList.add("hide");
+    } else {
+        header.classList.remove("hide");
+    }
+
+    lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+    if(window.scrollY >= 100) {
+        header.classList.add("active");
+        hideHeader();
+    } else {
+        header.classList.remove("active");
+    }
+})
