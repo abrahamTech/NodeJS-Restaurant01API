caeb4c8f876ea359a4defb7fcd2dc0c4f6278d44
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2024-03-19 00:05:12 -0600

    feat(About section): add parallax effect with js code

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -127,3 +127,29 @@ addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function
 addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
 
 window.addEventListener("load", autoSlide);
+
+
+/*
+************
+Parallax Effect
+************
+*/
+
+const parallaxItems = document.querySelectorAll("[data-parallax-item]")
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+    x = (event.clientX / window.innerWidth * 30) - 5;
+    y = (event.clientY / window.innerHeight * 30) - 5;
+
+    //Reverse the number
+    x = x - (x * 2);
+    y = y - (y * 2);
+
+    for (let i = 0, len = parallaxItems.length; i < len; i++) {
+        x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+        y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+        parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+    }
+})
